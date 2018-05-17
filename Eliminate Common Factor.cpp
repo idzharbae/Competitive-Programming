eab4722f0e3699b78602f2e319aed0eb986b1180
Muhammad Idzhar Darissalam <idzharbae@gmail.com>
2018-05-17 22:04:26 +0700

    Update Eliminate Common Factor.cpp

--- Eliminate Common Factor.cpp
+++ Eliminate Common Factor.cpp
@@ -8,14 +8,20 @@ typedef vector<int> vi;
 
 #define rep(i,a,n) for(int i = a; i < n; i++) 
 
-ll gcd(ll a, ll b) {
-    return (b == 0? a: gcd(b, a % b));
+ll gcd(ll a, ll b){
+	while(b){
+		ll temp = a;
+		a = b;
+		b = temp%b;
+	}
+	return a;
 }
-
 int main(){
-	int n; scanf("%d", &n);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n; cin>>n;
 	while(n--){
-		ll a,b,c;scanf("%I64d%I64d%I64d", &a,&b,&c);
+		ll a,b,c; cin>>a>>b>>c;
 		ll g = gcd(a,b); a /= g; b /= g;
 		while(g = gcd(b,c), g != 1){
 			while(b % g == 0) {
@@ -27,4 +33,3 @@ int main(){
 	
 	return 0;
 }
-
