51aeae72d45ad503751e6cfc2dc6d125c5aa67cd
Muhammad Idzhar Darissalam <idzharbae@gmail.com>
2018-05-28 20:57:32 +0700

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -8,3 +8,29 @@ int binser(vi arr, int val){
 	}
 	return mid;
 }
+
+// lower bound (not greater)
+int binser(vi arr, int val){
+	int l = 0, r = arr.size()-1,mid = (l+r)/2;;
+	while(l<r){
+		if(arr[mid] == val) return mid;
+		else if(val >= arr[mid]) l = mid+1;
+		else r = mid-1;
+		mid = (l+r)/2;
+	}
+	if(arr[mid] > val) mid--;
+	return mid;
+}
+
+//upper bound (greater)
+int binser(vi arr, int val){
+	int l = 0, r = arr.size()-1,mid = (l+r)/2;;
+	while(l<r){
+		if(arr[mid] == val) return mid+1;
+		else if(val >= arr[mid]) l = mid+1;
+		else r = mid-1;
+		mid = (l+r)/2;
+	}
+	if(arr[mid] <= val) mid++;
+	return mid;
+}
